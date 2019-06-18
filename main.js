c666866082e1dc30914893e73152cf338eb17948
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-06-18 14:35:21 +0200

    Add shapes populating logic

---
+++ main.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-multi-spaces */
+
+// CONTAINER SVG
+const svgNs = 'http://www.w3.org/2000/svg';
+const svgCanvas = document.createElementNS(svgNs, 'svg');
+svgCanvas.setAttribute('viewBox', '0 0 100 100');
+
+const shapesWidth = 10;
+const xColsOrigin = 18;
+let   xCols       = 18;
+let   yRows       = 18;
+
+for (let i = 0; i < 5; i += 1) {
+  for (let j = 0; j < 5; j += 1) {
+    if (i === j) {
+      const rectangle = document.createElementNS(svgNs, 'rect');
+      rectangle.setAttribute('x', xCols - 5);
+      rectangle.setAttribute('y', yRows - 5);
+      rectangle.setAttribute('width', shapesWidth);
+      rectangle.setAttribute('height', shapesWidth);
+      rectangle.setAttribute('stroke', '#33FF21');
+      rectangle.setAttribute('fill', 'none');
+      rectangle.setAttribute('id', `${i}${j}`);
+      if (i === 2 && j === 2) {
+        rectangle.setAttribute('transform', 'rotate(-45 48 48)');
+      }
+      svgCanvas.appendChild(rectangle);
+    } else {
+      const circle = document.createElementNS(svgNs, 'circle');
+      circle.setAttribute('cx', xCols);
+      circle.setAttribute('cy', yRows);
+      circle.setAttribute('r', shapesWidth / 2);
+      circle.setAttribute('stroke', 'blue');
+      circle.setAttribute('fill', 'none');
+      circle.setAttribute('id', `${i}${j}`);
+      svgCanvas.appendChild(circle);
+    }
+    xCols += 15;
+  }
+  xCols  = xColsOrigin;
+  yRows += 15;
+}
+
+document.getElementById('container').appendChild(svgCanvas);
